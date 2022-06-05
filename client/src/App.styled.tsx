3adcf12cde9c27a38e1537d90e15c877b7a096b9
Mateusz Kłusek <mateuszklusek@outlook.com>
2022-06-05 20:54:41 +0200

    fixed the problem when styling doesn't compile with styled components (removed @import inside styled component)

--- client/src/App.styled.tsx
+++ client/src/App.styled.tsx
@@ -2,7 +2,8 @@
 import styled from "styled-components"
 
 export const AppContainer = styled.div`
-    @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap");
+    /* NEVER IMPORT INSIDE STYLED COMPONENT !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! */
+    /* @import url("https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap"); */
     font-family: "Inter", sans-serif;
     width: 100%;
     min-height: 100vh;
