c64d65d76bd505e9dc60bd619ee431fb76e3fdba
Mateusz Kłusek <mateuszklusek@outlook.com>
2022-06-05 21:07:15 +0200

    fixed the changes to puppeteer to be able to launch browser on linux

--- server/src/helpers/scraper.js
+++ server/src/helpers/scraper.js
@@ -1,7 +1,7 @@
 import puppeteer from 'puppeteer';
 
 export const scrape = async (profileQuery) => {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] });
     const page = await browser.newPage();
     await page.goto(`https://github.com/${profileQuery}`);
 
